fa0b1f80042fb31e209c09901fce8b300aa959e8
Saurav Niroula <sauravniroula@192.168.101.6>
2026-01-09 22:51:30 +0545

    pay success fixed

--- forntend/src/pages/PaySuccess.jsx
+++ forntend/src/pages/PaySuccess.jsx
@@ -4,28 +4,6 @@ import { useSearchParams, useParams } from "react-router-dom"
 
 export default function PaySuccess() {
 
-    const [searchParams, setSearchParams] = useSearchParams()
-    const pidx=searchParams.get('pidx')
-
-    useEffect(()=>{
-        const confirmOrder=async()=>{
-            try {
-                const orderResponse=await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/order/orderIdFromPidx/${pidx}`,{
-                    headers:{
-                        'Authorization':`Bearer ${localStorage.getItem('token')}`
-                    }
-                })
-    
-                const orderId=orderResponse.data.data.orderId
-                console.log('orderId',orderId)
-                a
-            } catch (error) {
-                
-            }
-
-        }
-        confirmOrder()
-    })
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold text-center">Payment Successful</h1>
