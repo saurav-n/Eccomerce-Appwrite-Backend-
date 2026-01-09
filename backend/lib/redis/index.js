8676ad6c524e26ee5fe0ade4f6061ce84cd60222
Saurav Niroula <sauravniroula@192.168.101.6>
2026-01-09 21:30:28 +0545

    redis error fixed

--- backend/lib/redis/index.js
+++ backend/lib/redis/index.js
@@ -1,6 +1,5 @@
 import Redis from "ioredis";
 
-export const connection =new Redis({
-    host:process.env.REDIS_URL,
+export const connection =new Redis(process.env.REDIS_URL,{
     maxRetriesPerRequest:null
 })
